55f35ba1344756e24da6f4c81297f4a000c6e6b6
Paulo Valenzuela <paulovalensuela@hotmail.com>
2023-08-08 17:38:35 -0600

    Final

---
+++ src/assets/main.js
@@ -0,0 +1,43 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCGB9lFem70FKvphlf6NirZg&part=snippet%2Cid&order=date&maxResults=5';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'acb6f8728amsha7e2de7e66421b4p16cfe6jsna23afe1430b5',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-black">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+      `).slice(0,5).join('')}
+
+        `;
+        content.innerHTML = view;
+    } catch (error){
+        console.log(error)
+    }
+})();
